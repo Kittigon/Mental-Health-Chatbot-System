766b912b26e47835565d565db45ebe268af79940
Kittigon <bump8421@gmail.com>
2025-08-23 23:31:52 +0700

    edit-connSupabase

--- query_postgresql.py
+++ query_postgresql.py
@@ -7,7 +7,12 @@ import requests
 # Load environment variables
 load_dotenv()   
 
-SupabaseUrl = os.getenv("DATABASE_URL")
+# SupabaseUrl = os.getenv("DATABASE_URL")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")  
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
 CLOUDFLARE_ACCOUNT_ID = os.getenv("CLOUDFLARE_ACCOUNT_ID")
 CLOUDFLARE_API_TOKEN = os.getenv("CLOUDFLARE_API_TOKEN")
 
@@ -22,7 +27,13 @@ def get_embedding(text: str):
     return result["result"]["data"][0] 
 
 def query_postgresql(query_text, k=3):
-    conn = psycopg2.connect(SupabaseUrl)
+    conn = psycopg2.connect(
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        host=DB_HOST,
+        port=DB_PORT
+    )
     cur = conn.cursor()
 
     # แปลงข้อความเป็น embedding ผ่าน API
